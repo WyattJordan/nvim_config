10abde5b5726a04a5032fef8c15e9a56d5bb08f7
Wyatt Jordan <wyattsjordan@gmail.com>
2026-02-05 12:27:57 -0500

    customize with .is_server file

--- lua/plugins/disabled-plugins.lua
+++ lua/plugins/disabled-plugins.lua
@@ -1,5 +1,8 @@
+local is_server = vim.fn.filereadable(vim.fn.expand("~/.config/nvim/.is_server")) == 1
+
 return {
   { "akinsho/bufferline.nvim", enabled = false },
+  { "epwalsh/obsidian.nvim", enabled = not is_server },
 }
 --[[return {
   { "folke/persistence.nvim", enabled = false },
