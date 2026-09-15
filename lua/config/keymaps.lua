e16a218c728df97844ff1cce0b8f4ca58f3313d3
Wyatt Jordan <wyattsjordan@gmail.com>
2026-09-15 15:23:10 -0400

    code.amazon link yank

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,6 +21,27 @@ vim.keymap.set("n", "<leader>yf", function()
   vim.notify(name, vim.log.levels.INFO, { title = "Copied filename" })
 end, { desc = "Copy filename to clipboard" })
 
+-- Open a code.amazon.com link to the current line (package = git repo, branch = mainline)
+vim.keymap.set("n", "<leader>yl", function()
+  local root = vim.fs.root(0, ".git")
+  if not root then
+    vim.notify("Not inside a git repo", vim.log.levels.WARN, { title = "code.amazon link" })
+    return
+  end
+  local pkg = vim.fs.basename(root)
+  local rel = vim.fn.expand("%:p"):sub(#root + 2) -- strip root dir and its trailing slash
+  local url = string.format("https://code.amazon.com/packages/%s/blobs/mainline/--/%s#L%d", pkg, rel, vim.fn.line("."))
+  vim.fn.setreg("+", url)
+  vim.ui.open(url)
+  vim.notify(url, vim.log.levels.INFO, { title = "Opened code.amazon link" })
+end, { desc = "Open code.amazon link in browser" })
+
+-- Yank the current line without newline (important for grabbing CLI cmds)
+vim.keymap.set("n", "<leader>Y", function()
+  local line = vim.api.nvim_get_current_line()
+  vim.fn.setreg("+", line) -- Saves to the system clipboard (+)
+end, { desc = "Yank line text without newline" })
+
 vim.api.nvim_create_user_command("PasteHyperlinksAsMarkdown", function()
   local cmd = [[osascript -e 'the clipboard as «class HTML»' | ]]
     .. [[perl -ne 'print chr foreach unpack("C*",pack("H*",substr($_,11,-3)))' | ]]
